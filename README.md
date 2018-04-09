bb144aef1961e9a61fb48fdbf64e00397cbe23cf
Qingye Jiang <qjiang@ieee.org>
2018-04-09 04:01:39 +0000

    add logging to MySQL

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DistributedBenchmark
+
+
+
+~~~~
+$ mysql -u root -p
+mysql> CREATE DATABASE benchmark;
+mysql> CREATE USER 'username'@'%' IDENTIFIED BY 'password';
+mysql> GRANT ALL PRIVILEGES ON benchmark.* TO 'username'@'%';
+mysql> quit
+
+$ mysql -u username -p benchmark < benchmark.sql
+~~~~
--- src/main/java/net/qyjohn/DistributedBenchmark/Executor.java
+++ src/main/java/net/qyjohn/DistributedBenchmark/Executor.java
@@ -1,16 +1,38 @@
 package net.qyjohn.DistributedBenchmark;
 
 import java.io.*;
+import java.sql.*;
+import java.util.*;
 
 public class Executor extends Thread
 {
 	String node, command, outMsg, errMsg;
-	int exitVal;
+	String jdbcUrl;
+	Timestamp t0, t1;
+	int exitVal, duration;
 
 	public Executor(String node, String command)
 	{
 		this.node = node;
 		this.command = command;
+
+		try
+		{
+			// Getting database properties from db.properties
+			Properties prop = new Properties();
+			InputStream input = new FileInputStream("config.properties");
+			prop.load(input);
+			String dbHostname = prop.getProperty("dbHostname");
+			String dbUsername = prop.getProperty("dbUsername");
+			String dbPassword = prop.getProperty("dbPassword");
+			String dbDatabase = prop.getProperty("dbDatabase");
+
+			// Load the MySQL JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+			jdbcUrl = "jdbc:mysql://" + dbHostname + "/" + dbDatabase + "?user=" + dbUsername + "&password=" + dbPassword;
+		} catch (Exception e)
+		{
+		}
 	}
 
 	public void run()
@@ -30,14 +52,21 @@ public class Executor extends Thread
 	{
 		try
 		{
+			t0 = new Timestamp(new java.util.Date().getTime());
+			long t00 = System.currentTimeMillis();
+
 			Runtime rt = Runtime.getRuntime();
 			Process proc = rt.exec(command);
 			StreamGobbler stderr = new StreamGobbler(proc.getErrorStream());
 			StreamGobbler stdout = new StreamGobbler(proc.getInputStream());
 			stderr.start();
 			stdout.start();
-
 			exitVal = proc.waitFor();
+
+			t1 = new Timestamp(new java.util.Date().getTime());
+			long t01 = System.currentTimeMillis();
+			duration = (int) (t01 - t00);
+
 			outMsg = stdout.getOutput();
 			errMsg = stderr.getOutput();
 			System.out.println("        Exit Value: " + exitVal);
@@ -58,8 +87,23 @@ public class Executor extends Thread
 	{
 		try
 		{
+			// JDBC connection
+			Connection conn = DriverManager.getConnection(jdbcUrl);
+			String sql = "INSERT INTO logs (node, command, stdout, stderr, start_time, end_time, exit_value, duration) VALUES "
+				+ " (?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, node);
+			preparedStatement.setString(2, command);
+			preparedStatement.setString(3, outMsg);
+			preparedStatement.setString(4, errMsg);
+			preparedStatement.setTimestamp(5, t0);
+			preparedStatement.setTimestamp(6, t1);
+			preparedStatement.setInt(7, exitVal);
+			preparedStatement.setInt(8, duration);
+			preparedStatement.executeUpdate();
 		} catch (Exception e)
 		{
+			System.out.println(e.getMessage());
 		}		
 	}
 }
