3db555fa84cea8a597fb021a42c45125cfe31354
Qingye Jiang <qjiang@ieee.org>
2018-04-12 02:04:19 +0000

    updated speed test

--- src/main/java/net/qyjohn/DistributedBenchmark/SpeedTest.java
+++ src/main/java/net/qyjohn/DistributedBenchmark/SpeedTest.java
@@ -2,29 +2,36 @@ package net.qyjohn.DistributedBenchmark;
 
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class SpeedTest extends Thread
 {
 	String url;
-	
-	public SpeedTest(String url)
+	ConcurrentLinkedQueue<String> queue;
+
+	public SpeedTest(String url, ConcurrentLinkedQueue<String> queue)
 	{
 		this.url = url;
+		this.queue = queue;
 	}
 
 	public void run()
 	{
 		try
 		{
-                        byte[] buffer = new byte[1024*1024];
-                        InputStream in = new URL(url).openStream();
-                        int size = 0, length = 0;
-                        boolean go = true;
-                        while (size != -1)
-                        {
-                                size = in.read(buffer);
-                        }
-                        in.close();
+			// Repeat the download as long as there are jobs in the queue.
+			while (queue.poll() != null)
+			{
+		                byte[] buffer = new byte[1024*1024];
+		                InputStream in = new URL(url).openStream();
+		                int size = 0, length = 0;
+		                boolean go = true;
+		                while (size != -1)
+		                {
+		                        size = in.read(buffer);
+		                }
+		                in.close();
+			}
 		} catch (Exception e)
 		{
 		}
@@ -61,11 +68,18 @@ public class SpeedTest extends Thread
 			SpeedTest workers[] = new SpeedTest[nProc];			
 			for (int i=0; i<=repeat; i++)
 			{
+				int total = 32;
+				ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
+				for (int j=0; j<total; j++)
+				{
+					queue.add("job");
+				}
+
 				// Do this N times
 				time0 = System.currentTimeMillis();
 				for (int j=0; j<nProc; j++)
 				{
-					workers[j] = new SpeedTest(url);
+					workers[j] = new SpeedTest(url, queue);
 					workers[j].start();
 				}
 				for (int j=0; j<nProc; j++)
@@ -74,7 +88,7 @@ public class SpeedTest extends Thread
 				}
 				time1 = System.currentTimeMillis();
 				t = time1 - time0;
-				speed = nProc * (1000 * length / t) / 1024 / 1024;
+				speed = (total * length / t) / 1024;
 				System.out.println("Speed: " + speed + "MBps per second");
 			}
 		} catch (Exception e)
